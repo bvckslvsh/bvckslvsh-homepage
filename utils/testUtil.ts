1111e2aadc8ffe9b870704fb5a61df567e2664ef
Dima Kozyrev <bvckslvsh@gmail.com>
2025-02-15 12:11:35 +0300

    init dev

--- utils/testUtil.ts
+++ utils/testUtil.ts
@@ -2,6 +2,7 @@ export const testUtil = {
     generateRandomNumber(min: number, max: number) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     },
+
     capitalizeFirstLetter(str: string) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
